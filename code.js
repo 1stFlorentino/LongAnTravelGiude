ccac944d3272b4408fd9c611a03a62dc59dc5b66
1stFlorentino <128216532+1stFlorentino@users.noreply.github.com>
2026-02-14 23:27:46 +0700

    Add files via upload
    Upload lần 1
    Thêm được:
    + Trang chủ chọn địa điểm

---
+++ code.js
@@ -0,0 +1,13 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDCkEp8QtcpmkZAwlyM0lWxLaKkUQCQyls",
+    authDomain: "travelguide-f0904.firebaseapp.com",
+    projectId: "travelguide-f0904",
+    storageBucket: "travelguide-f0904.firebasestorage.app",
+    messagingSenderId: "674751697129",
+    appId: "1:674751697129:web:db6e4c30330cf53eb46078"
+  };
+
+  const app = initializeApp(firebaseConfig);
